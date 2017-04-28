1cb620baa88e85be3a043fd526d9d97254a15937
xinzhongtianxia <453935989@qq.com>
2017-04-28 20:18:17 +0800

    runtime beats 70.27% of java submissions

---
+++ src/com/tianxia/leetcode/StringToIntegerAtoi.java
@@ -0,0 +1,47 @@
+package com.tianxia.leetcode;
+
+public class StringToIntegerAtoi {
+	public int myAtoi(String str) {
+		if (str == null) {
+			return 0;
+		}
+		// whether to break from the loop if next char is not a digit
+		boolean isSafe = true;
+		int flag = 1;
+		int result = 0;
+		char[] arr = str.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (isSafe) {
+				if (arr[i] == ' ') {
+					continue;
+				}
+				if (arr[i] == '-') {
+					flag = -1;
+					isSafe = false;
+					continue;
+				}
+				if (arr[i] == '+') {
+					isSafe = false;
+					continue;
+				}
+
+				if (arr[i] > '9' || arr[i] < 0) {
+					return 0;
+				}
+				result = result * 10 + (int) (arr[i] - '0');
+				isSafe = false;
+			} else {
+				if (arr[i] > '9' || arr[i] < '0') {
+					break;
+				}
+				// overflow
+				if (result > 2147483647 / 10
+						|| ((result == 2147483647 / 10) && (arr[i] - '0') > 7)) {
+					return flag == 1 ? 2147483647 : -2147483648;
+				}
+				result = result * 10 + (int) (arr[i] - '0');
+			}
+		}
+		return result * flag;
+	}
+}
